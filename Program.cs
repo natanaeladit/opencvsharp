6df8191cb6834e868d6b4a8d15cd7cae4588947f
Natanael <natanaela@xrvision.ai>
2022-11-26 14:09:23 +0800

    add opencv

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Emgu.CV;
+using OpenCvSharp;
+
+try
+{
+    Console.WriteLine(CvInvoke.BuildInformation);
+
+    using var src = new OpenCvSharp.Mat("lena.png", ImreadModes.Grayscale);
+    using var dst = new OpenCvSharp.Mat();
+
+    Cv2.Canny(src, dst, 50, 200);
+    using (new Window("src image", src))
+    using (new Window("dst image", dst))
+    {
+        Cv2.WaitKey();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message + " " + ex.StackTrace);
+}
